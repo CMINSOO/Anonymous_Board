0867515ce68a62bfa33b982e32bcafadfc507caf
chominsoo <soochomin@gmail.com>
2024-10-10 02:22:14 +0900

    feat/회원가입 및 로그인

--- src/auth/auth.resolver.ts
+++ src/auth/auth.resolver.ts
@@ -1,27 +1,36 @@
 import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
 import { AuthService } from './auth.service';
 import { CreateAuthInput } from './dto/create-auth.input';
-import { UpdateAuthInput } from './dto/update-auth.input';
 import { User } from 'src/user/entities/user.entity';
+import { SignInInput } from './dto/sign-in.input';
+import { AuthPayload } from './object-types/auth-payload.type';
 
 @Resolver(() => User)
 export class AuthResolver {
   constructor(private readonly authService: AuthService) {}
 
+  //회원가입
   @Mutation(() => User)
   createUser(@Args('createAuthInput') createAuthInput: CreateAuthInput) {
     return this.authService.createUser(createAuthInput);
   }
 
-  // @Query('auth')
-  // findAll() {
-  //   return this.authService.findAll();
-  // }
+  //전체유저 조회
+  @Query(() => [User], { name: 'users' })
+  getAllUser() {
+    return this.authService.findAllUser();
+  }
 
-  // @Query('auth')
-  // findOne(@Args('id') id: number) {
-  //   return this.authService.findOne(id);
-  // }
+  @Mutation(() => AuthPayload, { name: 'signIn' })
+  async signIn(
+    @Args('signInInput') signInInput: SignInInput,
+  ): Promise<AuthPayload> {
+    const data = await this.authService.signIn(signInInput);
+    return {
+      user: data,
+      token: data.token,
+    };
+  }
 
   // @Mutation('updateAuth')
   // update(@Args('updateAuthInput') updateAuthInput: UpdateAuthInput) {
